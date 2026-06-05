c9cebd59108fb2805e56b82afacad8096f49e4e0
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2026-06-05 12:53:42 +0530

    removeing type errors, now phase 1 is complete-ish successfull build and local host

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+    allowedDevOrigins: [
+    "10.19.138.88",
+  ],
+
   images: {
     remotePatterns: [
       {
